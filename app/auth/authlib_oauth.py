0ede32d62c685b358d9adf3b7301804e1691cdc0
Julien Gaucher <julien.gaucher@csgroup.eu>
2024-07-29 16:46:26 +0200

    encrypt user_id and user_login in cookies

--- app/auth/authlib_oauth.py
+++ app/auth/authlib_oauth.py
@@ -17,8 +17,10 @@
 
 from authlib.integrations.starlette_client import OAuth
 from authlib.integrations.starlette_client.apps import StarletteOAuth2App
+from cryptography.fernet import Fernet
 from fastapi import APIRouter, FastAPI, HTTPException, status
 from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import HTMLResponse
 from starlette.config import Config
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
@@ -28,6 +30,10 @@ from ..settings import AuthInfo, api_settings
 from .keycloak_util import KCUtil
 
 keycloak: StarletteOAuth2App = None
+
+# Fernet guarantees that a message encrypted using it cannot be
+# manipulated or read without the key.
+fernet = Fernet(Fernet.generate_key())
 
 
 def init(app: FastAPI) -> APIRouter:
@@ -68,7 +74,7 @@ def init(app: FastAPI) -> APIRouter:
         ui_title = app.title + " - Swagger UI"
 
         # If the user is already logged in, do nothing, just display the Swagger UI.
-        if request.session.get("user"):
+        if request.session.get("user_id") and request.session.get("user_login"):
             return get_swagger_ui_html(openapi_url=app.openapi_url, title=ui_title)
 
         # Code and state coming from keycloak
@@ -82,22 +88,37 @@ def init(app: FastAPI) -> APIRouter:
             return await keycloak.authorize_redirect(request, redirect_uri)
 
         # Else we are called from keycloak.
-        # We save the user information received from keycloak.
-        #
-        # NOTE: in case of MismatchingStateError, normally this should not happen
-        # anymore. It may be a bug in Chrome linked to:
-        # https://github.com/encode/starlette/issues/2019
+        # We save and encrypt in cookies the user information received from keycloak.
         token = await keycloak.authorize_access_token(request)
         userinfo = dict(token["userinfo"])
-        request.session["user"] = userinfo
+        request.session["user_id"] = fernet.encrypt(userinfo["sub"].encode()).decode(
+            "utf-8"
+        )
+        request.session["user_login"] = fernet.encrypt(
+            userinfo["preferred_username"].encode()
+        ).decode("utf-8")
 
         # Redirect to this same endpoint to remove the URL query parameters
         return RedirectResponse("docs")
 
     @router.get("/logout")
     async def logout(request: Request):
-        request.session.pop("user", None)
-        return {"msg": "Logged out"}
+        request.session.pop("user_id", None)
+        request.session.pop("user_login", None)
+
+        for key in list(request.session.keys()):
+            if key.startswith("_state_"):
+                request.session.pop(key, None)
+
+        metadata = await keycloak.load_server_metadata()
+        end_session_endpoint = metadata["end_session_endpoint"]
+
+        return HTMLResponse(
+            "You are logged out.<br><br>"
+            "Click here to also log out from the authentication server: "
+            f"<a href='{end_session_endpoint}' target='_blank'>"
+            f"{end_session_endpoint}</a>"
+        )
 
     return router
 
@@ -106,8 +127,10 @@ kcutil = KCUtil()
 
 
 async def authlib_oauth(request: Request) -> AuthInfo:
-    user = request.session.get("user")
-    if not user:
+    # Read user information from cookies
+    user_id = request.session.get("user_id")
+    user_login = request.session.get("user_login")
+    if not (user_id and user_login):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail={
@@ -116,8 +139,8 @@ async def authlib_oauth(request: Request) -> AuthInfo:
             },
         )
 
-    user_id = user.get("sub")
-    user_login = user.get("preferred_username")
+    user_id = fernet.decrypt(user_id).decode()
+    user_login = fernet.decrypt(user_login).decode()
     user_info = kcutil.get_user_info(user_id)
 
     if user_info.is_enabled:
@@ -125,5 +148,5 @@ async def authlib_oauth(request: Request) -> AuthInfo:
     else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"User {user.get('preferred_username')!r} not found in keycloak.",
+            detail=f"User {user_login!r} not found in keycloak.",
         )

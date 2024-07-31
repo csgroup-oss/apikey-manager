d16103695304641077e4b78d5c99931374a9c19c
Julien Gaucher <julien.gaucher@csgroup.eu>
2024-07-31 14:16:16 +0200

    tests

--- app/auth/authlib_oauth.py
+++ app/auth/authlib_oauth.py
@@ -15,33 +15,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
+
 from authlib.integrations.starlette_client import OAuth
 from authlib.integrations.starlette_client.apps import StarletteOAuth2App
-from fastapi import APIRouter, FastAPI, HTTPException, status
+from fastapi import APIRouter, FastAPI, HTTPException, Request, Response, status
 from fastapi.responses import HTMLResponse
 from starlette.config import Config
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
-from urllib.parse import urlencode, urlparse, urlunparse, parse_qs
-from fastapi import FastAPI, Request, Response
-
 
 from ..settings import AuthInfo, api_settings
 from .keycloak_util import KCUtil
 
 keycloak: StarletteOAuth2App = None
 
+
 # See https://github.com/fastapi/fastapi/discussions/7817#discussioncomment-5144391
 class RequiresLoginException(Exception):
-    pass    
+    pass
 
 
 async def is_logged_in(request: Request) -> bool:
     return "user" in request.session
 
+
 async def console_logged_message() -> HTMLResponse:
     return HTMLResponse("You are logged in.")
+
 
 async def login(request: Request):
     """
@@ -49,14 +51,13 @@ async def login(request: Request):
     before displaying the Swagger UI.
     """
     calling_endpoint = request.url
-    called_from_console = (calling_endpoint.path.rstrip("/") == "/login_from_console")
+    called_from_console = calling_endpoint.path.rstrip("/") == "/login_from_console"
 
     # If the user is already logged in
     if await is_logged_in(request):
-
         if called_from_console:
             return await console_logged_message()
-    
+
         # If the login endpoint was called from the browser, redirect to the Swagger UI
         if calling_endpoint.path.rstrip("/") == "/login_from_browser":
             return RedirectResponse(request.app.docs_url)
@@ -90,7 +91,7 @@ async def login(request: Request):
     # See: https://stackoverflow.com/a/7734686
     url = urlparse(str(calling_endpoint))
     query = parse_qs(url.query, keep_blank_values=True)
-    for param in ['state', 'session_state', 'iss', 'code']:
+    for param in ["state", "session_state", "iss", "code"]:
         query.pop(param, None)
     url = url._replace(query=urlencode(query, True))
     return RedirectResponse(urlunparse(url))
@@ -120,7 +121,7 @@ def init(app: FastAPI) -> APIRouter:
         client_secret=config("KEYCLOAK_CLIENT_SECRET"),
         server_metadata_url=api_settings.oidc_metadata_url,
         client_kwargs={
-            "code_challenge_method": "S256", # Add PKCE for Authorization Code
+            "code_challenge_method": "S256",  # Add PKCE for Authorization Code
             "scope": "openid profile email",
         },
     )
@@ -138,9 +139,11 @@ def init(app: FastAPI) -> APIRouter:
         return await login(request)
 
     @app.exception_handler(RequiresLoginException)
-    async def exception_handler(request: Request, exc: RequiresLoginException) -> Response:
+    async def exception_handler(
+        request: Request, exc: RequiresLoginException
+    ) -> Response:
         return await login(request)
-    
+
     @router.get("/console_logged_message")
     async def console_logged_message_() -> HTMLResponse:
         return await console_logged_message()
@@ -162,50 +165,45 @@ def init(app: FastAPI) -> APIRouter:
             f"<a href='{end_session_endpoint}' target='_blank'>"
             f"{end_session_endpoint}</a>"
         )
-    
 
-
+    from typing import Annotated
 
     from fastapi import Depends
-    from typing import Annotated
     from pydantic import BaseModel
+
     class Item(BaseModel):
         name: str
         description: str | None = None
         price: float
         tax: float | None = None
+
     @router.post("/test_post")
     async def test_post(
-        auth_info: Annotated[AuthInfo, Depends(authlib_oauth)],
-        item: Item
+        auth_info: Annotated[AuthInfo, Depends(authlib_oauth)], item: Item
     ) -> str | None:
         bp = 0
-
-
 
     return router
 
 
 kcutil = KCUtil()
 
-    
-
 
 async def authlib_oauth(request: Request) -> AuthInfo:
     # Read user information from cookies to see if he's logged in
     user = request.session.get("user")
     if not user:
-
         # We can login then redirect to this endpoint, but this is not possible to make redirection from the Swagger.
         # In this case, referer = http://<domain>:<port>/docs
         referer = request.headers.get("referer")
         if referer and (urlparse(referer).path.rstrip("/") == request.app.docs_url):
             raise HTTPException(
-                status.HTTP_400_BAD_REQUEST, 
-                f"You must first login by calling this URL in your browser: {request.url_for('login_from_browser')}")
-        
+                status.HTTP_400_BAD_REQUEST,
+                f"You must first login by calling this URL in your browser: {request.url_for('login_from_browser')}",
+            )
+
         # Let's hope that the caller called from a browser (can we detect this ?) or the redirections won't work.
-        # Raising this exception will call the login method and redirect.        
+        # Raising this exception will call the login method and redirect.
         raise RequiresLoginException
 
     user_id = user.get("sub")
@@ -219,4 +217,3 @@ async def authlib_oauth(request: Request) -> AuthInfo:
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"User {user_login!r} not found in keycloak.",
         )
-
